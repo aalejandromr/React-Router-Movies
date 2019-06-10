5a1eeffe9c538a55f93ec982f7841df75c56cdfb
Jose Mejia <aalejandro.mreyes@icloud.com>
2019-06-10 18:51:56 -0400

    Refactor some code

--- client/src/App.js
+++ client/src/App.js
@@ -14,12 +14,7 @@ export default class App extends Component {
   }
 
   addToSavedList = movie => {
-    // const savedList = this.state.savedList;
-    // savedList.push(movie);
     this.setState(prevState => {
-      // if(prevState.savedList.map(s => s.title).includes(movie.title)) {
-      //   return prevState
-      // }
       if(prevState.savedList.filter(s => s.title === movie.title).length > 0) {
         return prevState;
       }
@@ -28,19 +23,6 @@ export default class App extends Component {
       }
     });
   };
-
-  // shouldComponentUpdate = (newProps, nexState) => {
-  //   // if(this.state.savedList.includes(currentList => currentList.id === nexState.savedList.id))
-  //   // console.log(this.state.savedList
-  //   //   .map(savedL => savedL.id)
-  //   //   .includes(nexState.savedList[nexState.savedList.length - 1]["id"]));
-  //   // console.log("Next", nexState.savedList);
-  //   // console.log(this.state.savedList.map(savedL => savedL.id).includes(nexState.savedList[nexState.savedList.length - 1]["id"]));   
-  //   // None of the above works
-
-  //   // lets try with set
-  //   return new Set(nexState.savedList.map(savedList => savedList.id)).size === this.state.savedList.map(savedList => savedList.id).length;
-  // }
 
   render() {
     return (
